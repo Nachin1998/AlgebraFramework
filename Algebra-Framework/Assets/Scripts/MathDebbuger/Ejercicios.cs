1eb0a2a21926d13b01a827dc78229063b2ad3375
Ignacio <Ignacio.f.l@hotmail.com>
2020-04-03 22:45:26 -0300

    moved entire prior project to this one. Completed some not impremented math, made ejercicio 1 & 2. Added an Ejercicio enumerator for easier access (More progress than expected)

---
+++ Algebra-Framework/Assets/Scripts/MathDebbuger/Ejercicios.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using MathDebbuger;
+using CustomMath;
+using System.Threading.Tasks;
+
+public class Ejercicios : MonoBehaviour
+{
+    public enum Ejercicio{
+        Uno,
+        Dos,
+        Tres,
+        Cuatro,
+        Cinco,
+        Seis,
+        Siete,
+        Ocho,
+        nueve,
+        Diez
+    };
+
+    public Ejercicio ejercicio = Ejercicio.Uno;
+    
+    public Vector3 lineaA;
+    public Vector3 lineaB;
+    Vec3 ejerResult;
+
+    void Start()
+    {
+        VectorDebugger.EnableCoordinates();
+
+        Vec3 a = new Vec3(lineaA);
+        Vec3 b = new Vec3(lineaB);
+
+        Debug.Log(lineaA.ToString());
+        VectorDebugger.AddVector(lineaA, Color.red, "Roja");
+        Debug.Log(lineaB.ToString());
+        VectorDebugger.AddVector(lineaB, Color.blue, "Azul");
+        Debug.Log(ejerResult.ToString());
+        VectorDebugger.AddVector(ejerResult, Color.white, "Blanca");
+
+        VectorDebugger.EnableEditorView();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            VectorDebugger.TurnOffVector("elAzul");
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            VectorDebugger.TurnOnVector("elAzul");
+        }
+
+        switch (ejercicio)
+        {
+            case Ejercicio.Uno:
+                ejerResult = new Vec3(lineaA + lineaB);
+                break;
+            case Ejercicio.Dos:
+                ejerResult = new Vec3(lineaB - lineaA);
+                break;
+            case Ejercicio.Tres:
+                break;
+            case Ejercicio.Cuatro:
+                break;
+            case Ejercicio.Cinco:
+                break;
+            case Ejercicio.Seis:
+                break;
+            case Ejercicio.Siete:
+                break;
+            case Ejercicio.Ocho:
+                break;
+            case Ejercicio.nueve:
+                break;
+            case Ejercicio.Diez:
+                break;
+            default:
+                Debug.Log("Algebra machine broke");
+                break;
+        }
+
+        VectorDebugger.UpdatePosition("Roja", lineaA);
+        VectorDebugger.UpdatePosition("Azul", lineaB);
+        VectorDebugger.UpdatePosition("Blanca", ejerResult);
+    }
+}
---
+++ Algebra-Framework/Assets/Scripts/MathDebbuger/Tests/Tester.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using MathDebbuger;
+using CustomMath;
+
+public class Tester : MonoBehaviour
+{
+    public Vector3 lineaA;
+    public Vector3 lineaB;
+    Vec3 ejer1Result;
+
+    void Start()
+    {
+        VectorDebugger.EnableCoordinates();
+
+        Vec3 a = new Vec3(lineaA);
+        Vec3 b = new Vec3(lineaB);
+
+        Debug.Log(lineaA.ToString());
+        VectorDebugger.AddVector(lineaA, Color.red, "Roja");
+        Debug.Log(lineaB.ToString());
+        VectorDebugger.AddVector(lineaB, Color.blue, "Azul");
+        Debug.Log(ejer1Result.ToString());
+        VectorDebugger.AddVector(ejer1Result, Color.white, "Blanca");
+
+        VectorDebugger.EnableEditorView();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            VectorDebugger.TurnOffVector("elAzul");
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            VectorDebugger.TurnOnVector("elAzul");
+        }
+
+        ejer1Result = new Vec3(lineaA + lineaB);
+        VectorDebugger.UpdatePosition("Roja", lineaA);
+        VectorDebugger.UpdatePosition("Azul", lineaB);
+        VectorDebugger.UpdatePosition("Blanca", ejer1Result);
+    }
+}
