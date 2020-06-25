d64c2d226ef6480e2586b3ca3cb3ba11d63570a5
Ignacio <Ignacio.f.l@hotmail.com>
2020-06-25 20:02:40 -0300

    Completed MyMatrix4x4 constructor

--- Assets/Scripts/Matrix4x4/MyMatrix4x4.cs
+++ Assets/Scripts/Matrix4x4/MyMatrix4x4.cs
@@ -1,33 +1,50 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using CustomMath;
-using System.Runtime.CompilerServices;
 
 namespace CustomMath {
     public class MyMatrix4x4 : MonoBehaviour
     {
         public float m00;
-        public float m33;
-        public float m23;
-        public float m13;
-        public float m03;
-        public float m32;
-        public float m22;
-        public float m02;
-        public float m12;
-        public float m21;
-        public float m11;
         public float m01;
-        public float m30;
-        public float m20;
+        public float m02;
+        public float m03;
+
         public float m10;
+        public float m11;
+        public float m12;
+        public float m13;
+        
+        public float m20;
+        public float m21;
+        public float m22;
+        public float m23;
+        
+        public float m30;
         public float m31;
+        public float m32;
+        public float m33;
 
         public MyMatrix4x4(Vector4 column0, Vector4 column1, Vector4 column2, Vector4 column3)
         {
-            throw new NotImplementedException();
+            m00 = column0.x;
+            m10 = column0.y;
+            m20 = column0.z;
+            m30 = column0.w;
+            
+            m01 = column1.x;
+            m11 = column1.y;
+            m21 = column1.z;
+            m31 = column1.w;
+            
+            m02 = column2.x;
+            m12 = column2.y;
+            m22 = column2.z;
+            m32 = column2.w;
+            
+            m03 = column3.x;
+            m13 = column3.y;
+            m23 = column3.z;
+            m33 = column3.w;
         }
 
         public static MyMatrix4x4 Rotate(Quaternion q)
--- Assets/Scripts/Quaternions/QuatRespuestas.cs
+++ Assets/Scripts/Quaternions/QuatRespuestas.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections;
-using System.Text.RegularExpressions;
-using UnityEngine;
+﻿using UnityEngine;
 using CustomMath;
-using UnityEngine.UIElements;
-using System.Threading;
+using MathDebbuger;
+using EjerciciosAlgebra;
 
 public class QuatRespuestas : MonoBehaviour
 {
@@ -13,21 +11,26 @@ public class QuatRespuestas : MonoBehaviour
         Dos,
         Tres
     }
-    
+
     public RespuestaAEjer respuestaAEjer = RespuestaAEjer.Uno;
 
+    public GameObject thing;
     public float angle;
-    public Quaternion q1 = new Quaternion(1, 1, 1, 1);
-    public Quaternions qc = new Quaternions(1, 1, 1, 1);
-    float t;
+    Quaternion q = new Quaternion(1, 1, 1, 1);
+    Quaternions q1 = new Quaternions(1, 1, 1, 1);
+    Quaternions q2 = new Quaternions(1, 1, 1, 1);
+    Quaternions q3 = new Quaternions(1, 1, 1, 1);
+    float timer;
 
     // Update is called once per frame
     private void Start()
     {
-        Debug.Log(qc + " test");
+        //Debug.Log(qc + " test");
     }
     void Update()
     {
+        angle = Quaternions.Angle(q2, q3);
+        thing.transform.rotation = new Quaternion(0, 0, angle, 0);
         switch (respuestaAEjer)
         {
             case RespuestaAEjer.Uno:
